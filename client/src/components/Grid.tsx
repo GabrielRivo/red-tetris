912a3328d8cbc59f4bc2d3ee239e2664e98c5e28
GabrielRivo <gabriel.rivo95@gmail.com>
2026-07-25 22:45:40 +0200

    Fixed vertical lines disappearing due to responsive behaviors

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -2,32 +2,34 @@ import { GRID_STATES } from "../../../shared/types";
 import { cellColor } from "./types";
 
 type GridProps = {
-  grid: GRID_STATES[][];
-  gridColsClass: string;
-  gridRowsClass: string;
-  cellSizeClass: string;
+    grid: GRID_STATES[][];
+    gridColsClass: string;
+    gridRowsClass: string;
+    cellSizeClass: string;
 };
 
 function Grid({
-  grid,
-  gridColsClass,
-  gridRowsClass,
-  cellSizeClass,
+    grid,
+    gridColsClass,
+    gridRowsClass,
+    cellSizeClass,
 }: GridProps) {
-  return (
-    <div
-      className={`grid ${gridColsClass} ${gridRowsClass} border-l border-t border-white/50 select-none cursor-default`}
-    >
-      {grid.map((row, rowIndex) =>
-        row.map((cell, colIndex) => (
-          <div
-            key={`${rowIndex}-${colIndex}`}
-            className={`border-r border-b border-white/50 ${cellSizeClass} ${cellColor[cell]}`}
-          />
-        )),
-      )}
-    </div>
-  );
+    return (
+        <div
+            className={`grid ${gridColsClass} ${gridRowsClass} border-t border-white/50 select-none cursor-default`}
+        >
+            {grid.map((row, rowIndex) =>
+                row.map((cell, colIndex) => (
+                    <div
+                        key={`${rowIndex}-${colIndex}`}
+                        className={`border-r border-b border-white/50 ${
+                            colIndex === 0 ? "border-l" : ""
+                        } ${cellSizeClass} ${cellColor[cell]}`}
+                    />
+                )),
+            )}
+        </div>
+    );
 }
 
 export default Grid;
